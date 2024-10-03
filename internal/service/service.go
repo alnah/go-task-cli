2c317f806c3d05468dfe173c3ca84081f63393d7
alnah <alexis.nahan@gmail.com>
2024-10-03 13:48:20 -0300

    feat: implement service to pass all tests

---
+++ internal/service/service.go
@@ -0,0 +1,109 @@
+package service
+
+import (
+	"io"
+
+	r "github.com/alnah/task-tracker/internal/repository"
+	s "github.com/alnah/task-tracker/internal/storage"
+)
+
+type Service interface {
+	AddTask(description string) (r.Task, error)
+	UpdateTask(id uint, description *string, status *r.Status) (r.Task, error)
+	DeleteTask(id uint) (bool, error)
+	ListTasks(status *r.Status) (r.Tasks, error)
+}
+
+type TaskService struct {
+	Reader     io.Reader
+	Writer     io.Writer
+	Storage    s.Storage
+	Repository r.Repository
+}
+
+type UpdateTaskParams struct {
+	ID          uint
+	Description *string
+	Status      *r.Status
+}
+
+func (s *TaskService) AddTask(description string) (r.Task, error) {
+	if _, loadErr := s.loadAndImportTasks(); loadErr != nil {
+		return r.Task{}, loadErr
+	}
+
+	task, addErr := s.Repository.AddTask(description)
+	if addErr != nil {
+		return r.Task{}, addErr
+	}
+
+	_, saveErr := s.Storage.SaveTasks(s.Writer, s.Repository.FindAll())
+	if saveErr != nil {
+		return r.Task{}, saveErr
+	}
+
+	return task, nil
+}
+
+func (s *TaskService) UpdateTask(params UpdateTaskParams) (r.Task, error) {
+	if _, loadErr := s.loadAndImportTasks(); loadErr != nil {
+		return r.Task{}, loadErr
+	}
+
+	updateParams := r.UpdateTaskParams{
+		ID:          params.ID,
+		Description: params.Description,
+		Status:      params.Status,
+	}
+
+	task, updateErr := s.Repository.UpdateTask(updateParams)
+	if updateErr != nil {
+		return r.Task{}, updateErr
+	}
+
+	_, saveErr := s.Storage.SaveTasks(s.Writer, s.Repository.FindAll())
+	if saveErr != nil {
+		return r.Task{}, saveErr
+	}
+
+	return task, nil
+}
+
+func (s *TaskService) DeleteTask(id uint) (bool, error) {
+	if _, loadErr := s.loadAndImportTasks(); loadErr != nil {
+		return false, loadErr
+	}
+
+	if _, deleteErr := s.Repository.DeleteTask(id); deleteErr != nil {
+		return false, deleteErr
+	}
+
+	_, saveErr := s.Storage.SaveTasks(s.Writer, s.Repository.FindAll())
+	if saveErr != nil {
+		return false, saveErr
+	}
+
+	return true, nil
+}
+
+func (s *TaskService) ListTasks(status *r.Status) (r.Tasks, error) {
+	if _, loadErr := s.loadAndImportTasks(); loadErr != nil {
+		return r.Tasks{}, loadErr
+	}
+
+	if status != nil {
+		return s.Repository.FindMany(*status), nil
+	}
+
+	return s.Repository.FindAll(), nil
+}
+
+func (s *TaskService) loadAndImportTasks() (r.Tasks, error) {
+	loadedTasks, loadErr := s.Storage.LoadTasks(s.Reader)
+	if loadErr != nil {
+		return r.Tasks{}, loadErr
+	}
+
+	tasks := s.Repository.ImportTasksData(loadedTasks)
+	return tasks, nil
+}
